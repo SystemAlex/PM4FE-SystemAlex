6a8cb26646313c78850c53cca78f25d9d60e24b9
SystemAlex <alexandrobeese@hotmail.com>
2025-01-22 21:27:17 -0300

    Prueba

--- front/intrefaces/Order.ts
+++ front/intrefaces/Order.ts
@@ -1,6 +1,5 @@
 import { User } from './User';
 import { Products } from './Products';
-
 export interface Order {
   id: number,
   status: string
--- front/intrefaces/User.ts
+++ front/intrefaces/User.ts
@@ -3,7 +3,6 @@ export interface User extends UserLoginData {
     address: string,
     phone: string
 }
-
 export interface UserLoginData {
     name: string,
     email: string
